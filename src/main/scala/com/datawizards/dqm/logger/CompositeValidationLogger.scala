52c157130343c3a5b7f7a619db1b23cdb9543905
piotr-kalanski <piotr.kalanski@gmail.com>
2017-09-12 17:04:57 +0200

    Validation Result Logger - composite resolves #31

---
+++ src/main/scala/com/datawizards/dqm/logger/CompositeValidationLogger.scala
@@ -0,0 +1,20 @@
+package com.datawizards.dqm.logger
+
+import com.datawizards.dqm.result._
+
+class CompositeValidationLogger(loggers: Seq[ValidationResultLogger]) extends ValidationResultLogger {
+
+  override def log(result: ValidationResult): Unit =
+    loggers.foreach(_.log(result))
+
+  override protected def logInvalidRecords(invalidRecords: Seq[InvalidRecord]): Unit = { /* do nothing */ }
+
+  override protected def logTableStatistics(tableStatistics: TableStatistics): Unit = { /* do nothing */ }
+
+  override protected def logColumnStatistics(columnsStatistics: Seq[ColumnStatistics]): Unit = { /* do nothing */ }
+
+  override protected def logGroupByStatistics(groupByStatisticsList: Seq[GroupByStatistics]): Unit = { /* do nothing */ }
+
+  override protected def logInvalidGroups(invalidGroups: Seq[InvalidGroup]): Unit = { /* do nothing */ }
+
+}
