8031d08bfa9cd0f01629d4a9a41a686cb7d84199
piotr-kalanski <piotr.kalanski@gmail.com>
2017-09-15 15:54:45 +0200

    FileConfigurationLoader - with single table configuration resolves #48

---
+++ src/main/scala/com/datawizards/dqm/configuration/loader/FileMultipleTablesConfigurationLoader.scala
@@ -0,0 +1,71 @@
+package com.datawizards.dqm.configuration.loader
+
+import java.io.File
+
+import com.datawizards.dqm.configuration.DataQualityMonitoringConfiguration
+import com.typesafe.config.{Config, ConfigFactory}
+
+/**
+  * Loads multiple tables configuration from file.
+  * <br/>
+  * Expected format:
+  * <pre>
+  *tablesConfiguration = [
+    {
+      location = {type = Hive, table = clients},
+      rules = {
+        rowRules = [
+          {
+            field = client_id,
+            rules = [
+              {type = NotNull},
+              {type = min, value = 0}
+            ]
+          },
+          {
+            field = client_name,
+            rules = [
+              {type = NotNull}
+            ]
+          }
+        ]
+      }
+    },
+    {
+      location = {type = Hive, table = companies},
+      rules = {
+        rowRules = [
+          {
+            field = company_id,
+            rules = [
+              {type = NotNull},
+              {type = max, value = 100}
+            ]
+          },
+          {
+            field = company_name,
+            rules = [
+              {type = NotNull}
+            ]
+          }
+        ]
+      }
+    }
+  ]
+  * <pre>
+  *
+  * @param path configuration file
+  */
+class FileMultipleTablesConfigurationLoader(path: String) extends ConfigurationLoader {
+
+  override def loadConfiguration(): DataQualityMonitoringConfiguration = {
+    val config = ConfigFactory.parseFile(new File(path))
+    parseConfig(config)
+  }
+
+  private def parseConfig(config: Config): DataQualityMonitoringConfiguration = {
+    val tablesConfiguration = config.getList("tablesConfiguration")
+    DataQualityMonitoringConfiguration(parseTablesConfiguration(tablesConfiguration))
+  }
+
+}
